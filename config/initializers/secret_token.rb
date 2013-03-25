5802c374478e391c7485d6ee994fbc27be2f4c11
jarrielcook <jarriel_cook@yahoo.com>
2013-03-24 22:03:42 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gravtate::Application.config.secret_token = '06c5b55d1215c0e106592406e851ed89e1d506b609ef85420f810f97405f11896a2f15b90ff91108bfd9a2199647077201331802cf6e48e54243bde76ecf16f5'
